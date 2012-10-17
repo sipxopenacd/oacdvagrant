46e9ddfc19fe8befb836b7b6d3b4ad9993fc39dd
Jan Vincent Liwanag <jvliwanag@gmail.com>
2012-10-17 22:01:10 +0800

    Login accepts username password
    Mechanize instance moved as last param

--- conf/oadevenv.rb
+++ conf/oadevenv.rb
@@ -6,15 +6,17 @@ $admin_password = 'superadmin'
 
 $sipxconfig_url = 'http://10.24.7.87:12000/sipxconfig/'
 
-def login(a=nil)
+def login(username=nil, password=nil, a=nil)
 	a = Mechanize.new if a.nil?
+	username = $admin_username if username.nil?
+	password = $admin_password if password.nil?
 
 	login_p = a.get($sipxconfig_url)
 	login_f = login_p.form_with(:action => "/sipxconfig/j_acegi_security_check")
 
 	throw :login_not_found if login_f.nil?
 
-	login_f['j_username'] = $admin_username
-	login_f['j_password'] = $admin_password
+	login_f['j_username'] = username
+	login_f['j_password'] = password
 	login_f.submit
 end
